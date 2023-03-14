b0a3dc0e15c43d68cc3b41c6b651e8b7a5464934
Juraj Piovar <>
2023-03-14 14:12:45 +0100

    redux

--- app1/src/pages/home/Home.js
+++ app1/src/pages/home/Home.js
@@ -1,19 +1,33 @@
 import './Home.scss';
 import { Button } from '@carbon/react';
+import { decrement, increment } from 'redux/counter';
+import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
 
 const Home = () => {
 
-  const shoot = () => {
-    console.log('Great Shot!');
+  // @ts-ignore
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    console.log('increment Great Shot!');
+    dispatch(increment());
+  };
+
+  const decrease = () => {
+    console.log('decrement Great Shot!');
+    dispatch(decrement());
   };
 
   return (
     <div>
       <h1 id="home">home page</h1>    
       <div>
-        <Button onClick={shoot}>content load</Button>
+        <Button onClick={increase}>increase content load</Button>
+        <Button onClick={decrease}>decrease content load</Button>
       </div>  
+      <span>{count}</span>
     </div>
   );
 };
