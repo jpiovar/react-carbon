029aba426029c16597d8ed15c5cb393333925fd6
Juraj Piovar <>
2023-03-16 11:58:23 +0100

    saga working version

--- app1/src/state/user/user.sagas.js
+++ app1/src/state/user/user.sagas.js
@@ -1,22 +1,28 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { GET_USER_FETCH, GET_USER_SUCCESS, GET_USER_FAIL } from './user.actions';
 
+/* eslint-disable no-debugger */
 function userFetch(userId) {
+  debugger;
   console.log({userId});
   return fetch('http://localhost:3001/user')
     .then(response => response.json());
 }
 
-function* fetchUser(action) {
+/* eslint-disable no-debugger */
+function* fetchUser() {
+  debugger;
   try {
-    const user = yield call(userFetch, action.payload.userId);
+    const user = yield call(userFetch);
     yield put({type: GET_USER_SUCCESS, user: user});
   } catch (e) {
     yield put({type: GET_USER_FAIL, message: e.message});
   }
 }
 
+/* eslint-disable no-debugger */
 function* userSaga() {
+  debugger;
   yield takeEvery(GET_USER_FETCH, fetchUser);
 }
 
