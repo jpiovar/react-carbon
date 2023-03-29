a01cf8d5d3455eb2cd967ef215039fd3e6b78dd6
Juraj Piovar <>
2023-03-29 11:35:35 +0200

    webpack sass loader node modules

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,27 +1,32 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./app/entry", // string | object | array
+  entry: './app/entry', // string | object | array
   // Here the application starts executing
   // and webpack starts bundling
   output: {
     // options related to how webpack emits results
-    path: path.resolve(__dirname, "dist"), // string
+    path: path.resolve(__dirname, 'dist'), // string
     // the target directory for all output files
     // must be an absolute path (use the Node.js path module)
-    filename: "bundle.js", // string
-      // the filename template for entry chunks
-    },
-    module: {
-      rules: [{
-          test: /\.scss$/,
-            use: [{
-              loader: "style-loader"
-            }, {
-              loader: "css-loader" 
-            }, {
-              loader: "sass-loader"
-            }]
+    filename: 'bundle.js', // string
+    // the filename template for entry chunks
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            includePaths: ['node_modules']
+          }
+        },
       }]
-    }
+    }]
+  }
 }
