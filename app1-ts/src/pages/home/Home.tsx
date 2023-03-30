74de28e084880093d7b53e5b8693f9bbce21c817
Juraj Piovar <>
2023-03-30 15:52:21 +0200

    carbon grid basic

--- app1-ts/src/pages/home/Home.tsx
+++ app1-ts/src/pages/home/Home.tsx
@@ -4,6 +4,8 @@ import { Add } from '@carbon/react/icons';
 import React from 'react';
 import { Button } from 'carbon-components-react';
 
+import { Grid, Column } from '@carbon/react';
+
 const Home = (): JSX.Element => {
 
   const shoot = (): void => {
@@ -12,11 +14,46 @@ const Home = (): JSX.Element => {
 
   return (
     <div>
-      <h1 id="home">home page</h1>    
+      <h1 id="home">home page</h1>
       <div>
         <Add />
         <Button onClick={shoot}><Add />content load</Button>
-      </div>  
+      </div>
+      <br/>
+      <br/>
+      <div>
+        <Grid className="landing-page" fullWidth>
+          <Column lg={16} md={8} sm={4} className="landing-page__banner">
+            1
+          </Column>
+          <Column lg={16} md={8} sm={4} className="landing-page__r2">
+            <Grid className="tabs-group-content">
+              <Column md={4} lg={7} sm={4} className="landing-page__tab-content">
+                7/16
+              </Column>
+              <Column md={4} lg={{ span: 8, offset: 7 }} sm={4}>
+                8/16
+              </Column>
+            </Grid>
+          </Column>
+          <Column lg={16} md={8} sm={4} className="landing-page__r3">
+            <Grid>
+              <Column md={4} lg={4} sm={4}>
+                1/4
+              </Column>
+              <Column md={4} lg={4} sm={4}>
+                1/4
+              </Column>
+              <Column md={4} lg={4} sm={4}>
+                1/4
+              </Column>
+              <Column md={4} lg={4} sm={4}>
+                1/4
+              </Column>
+            </Grid>
+          </Column>
+        </Grid>
+      </div>
     </div>
   );
 };
